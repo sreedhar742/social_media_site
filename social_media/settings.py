2b3fdb57301ee45b88b3e635a4ab688a73789008
sreedhar742 <psridhar742@gmail.com>
2024-12-22 13:03:22 +0530

    updated vercel.json

--- social_media/settings.py
+++ social_media/settings.py
@@ -133,16 +133,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ['.map']
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'libs/static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
-# Use simple storage backend instead of CompressedManifestStaticFilesStorage
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
